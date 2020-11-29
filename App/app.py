13b8dbb9d04a6ede379a4b639f085f3dbb84255d
castafra <castafra@users.noreply.github.com>
2020-11-29 10:52:02 +0100

    Update app.py

--- App/app.py
+++ App/app.py
@@ -16,20 +16,43 @@ df = pd.DataFrame({
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
+df2 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+markdown_text = '''
+This application was developped as part of a end of study projet by Mehdi A. and François C., student at CentraleSupelec
+'''
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='AI Compression Toolbox', style={
+        'textAlign': 'center'}),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+        Upload an image too see how the compression works
+    ''', style={
+        'textAlign': 'center'}),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
-    )
+    ),
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df2),
+    dcc.Markdown(children=markdown_text)
 ])
 
 if __name__ == '__main__':
