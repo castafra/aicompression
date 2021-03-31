96ca837dde5702317dce459fa75152ab6cb37d4a
castafra <68732020+castafra@users.noreply.github.com>
2021-03-31 20:18:42 +0200

    ADD OCR in pipeline

--- src/test.py
+++ src/test.py
@@ -1,15 +1,18 @@
 from pipeline import *
 from PIL import Image
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\François\Documents\GitHub\tesseract'
 
 
-"""
+
+
 compression = compressor(PATH_TO_OD_LABELS="../models/object_detection/labels",PATH_TO_OD_MODEL_DIR="../models/object_detection")
 
-detections = compression.detect_objects("C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\explanation.jpg")
+detections = compression.detect_objects("C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\slide_4.jpg")
 
-compression.visualize_detections()
-"""
-img = Image.open('C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\explanation.jpg')
-print(pytesseract.image_to_string(img))
+#compression.visualize_detections()
+
+#compression.background_retrieve()
+
+compression.perform_ocr()
+
+#img = Image.open(r'C:\Users\François\Desktop\text_sample.jpg')
+#print(pytesseract.image_to_string(img))
