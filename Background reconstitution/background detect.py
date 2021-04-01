610db416ee455a264d7cf0d882b20b1428ea95b4
arsayes <75136959+arsayes@users.noreply.github.com>
2021-04-01 14:54:59 +0200

    Update background detect.py

--- Background reconstitution/background detect.py
+++ Background reconstitution/background detect.py
@@ -14,7 +14,9 @@ import pandas as pd
        2, 0, 1, 2, 2, 2, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 2, 2, 0,
        0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 2, 0]'''
 
-data = {'box': [[223,287,707,655],[274,1117,530,1749], [130, 1291, 210, 1390]], 'class': ['image', 'text', 'logo']}
+#data = {'box': [[223,287,707,655],[274,1117,530,1749], [130, 1291, 210, 1390]], 'class': ['image', 'text', 'logo']}
+data = {'box': [[223,287,707,655],[230,817,530,1749], [120, 1291, 210, 1390]], 'class': ['image', 'text', 'logo']}
+
 df_test= pd.DataFrame(data)
 img=Image.open("C:\\Users\\samze\\Pictures\\test.png")
 #output = [[ymin1,xmin1,ymax1,xmax1],...]
@@ -73,17 +75,18 @@ def remove_elements_0(out, slide, classes):
 
 
 def fill_black_1(detected_objects, image):
+    #hypotheses: boxes well detected, one band per box
     draw_slide=ImageDraw.Draw(image)
     for i in range(len(detected_objects)):
         x=detected_objects['box'].loc[i][1]
         y=detected_objects['box'].loc[i][0]
-        r0, b0, g0 = image.getpixel(x,y)
+        r0, b0, g0 = image.getpixel((x-1,y-1))
         vert_band_not_found = True
         hor_band_not_found = True
         #hypothesis : max 1 color band per box
         while x< detected_objects['box'].loc[i][3] and vert_band_not_found:
-            r, g, b = image.getpixel((x, y))
-            vert_band_not_found=vert_band_not_found and (r, g, b == image.getpixel((x+1, y)))
+            #r, g, b = image.getpixel((x, y))
+            vert_band_not_found=vert_band_not_found and (image.getpixel((x, y)) == image.getpixel((x+1, y)))
             x+=1
         if vert_band_not_found:
             """draw_slide.rectangle([(detected_objects['box'].loc[i][1],detected_objects['box'].loc[i][0]),
@@ -91,8 +94,8 @@ def fill_black_1(detected_objects, image):
         #add vertical band research
         else:"""
             while y< detected_objects['box'].loc[i][2] and hor_band_not_found:
-                r, g, b = image.getpixel((x, y))
-                hor_band_not_found = hor_band_not_found and (r, g, b == image.getpixel((x, y+1)))
+                #r, g, b = image.getpixel((x, y))
+                hor_band_not_found = hor_band_not_found and (image.getpixel((x, y)) == image.getpixel((x, y+1)))
                 y+=1
             if not hor_band_not_found:
                 draw_slide.rectangle([(detected_objects['box'].loc[i][1],
@@ -100,33 +103,35 @@ def fill_black_1(detected_objects, image):
                                               (detected_objects['box'].loc[i][3],
                                                    y)],
                                                        (r0, b0, g0) )
-                draw_slide.rectangle([(detected_objects['box'].loc[i][1],y+1),
+                
+                draw_slide.rectangle([(detected_objects['box'].loc[i][1],y),
                                           (detected_objects['box'].loc[i][3],
-                                               detected_objects['box'].loc[i][2])],
-                                                    (r,g,b) )
+                                               detected_objects['box'].loc[i][2])], outline = '#000000',
+                                                    fill = image.getpixel((x+1, y+2)) )
+                print(x,y, 'horizontal found')
             else: #a check
                 draw_slide.rectangle([(detected_objects['box'].loc[i][1],
                                  detected_objects['box'].loc[i][0]),
                                      (x, detected_objects['box'].loc[i][2])],
-                                         (r0,g0,b0) )
+                                         fill = image.getpixel((x,y)) )
+                print(x,y, 'horizontal and vertical not found')
                 
         else:
             draw_slide.rectangle([(detected_objects['box'].loc[i][1],
                                  detected_objects['box'].loc[i][0]),
                                      (x, detected_objects['box'].loc[i][2])],
                                          (r0,g0,b0) )
-
-            r,g,b= image.getpixel((x+1, y))
-            draw_slide.rectangle([(x+1,detected_objects['box'].loc[i][0]),
+            print(x,y, 'vertical found')
+            draw_slide.rectangle([(x,detected_objects['box'].loc[i][0]),
                                   (detected_objects['box'].loc[i][3], 
-                                   detected_objects['box'].loc[i][2])] , (r,g,b) )
+                                   detected_objects['box'].loc[i][2])] , image.getpixel((x+1, y)) )
         
 
         image.show()
     return None        
         
 point_table = ([0] + ([255] * 255))
-
+'''
 def black_or_b(a, b):
     diff = ImageChops.difference(a, b)
     diff = diff.convert('L')
@@ -139,7 +144,7 @@ a = Image.open('a.png')
 b = Image.open('b.png')
 c = black_or_b(a, b)
 c.save('c.png')
-
+'''
 
 
 
