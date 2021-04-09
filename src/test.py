1621fd3c5f23cbf2058d821d9b0d792c8ad7ec75
castafra <68732020+castafra@users.noreply.github.com>
2021-04-09 14:13:39 +0200

    ADD text images generation

--- src/test.py
+++ src/test.py
@@ -17,6 +17,14 @@ from PIL import Image
 
 
 if __name__ == '__main__':
-    compare_image = CompareImage('C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\slide_4.jpg', 'C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\slide_4.jpg')
-    image_difference = compare_image.compare_image()
-    print(image_difference)
+    #compare_image = CompareImage('C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\slide_4.jpg', 'C:\\Users\\François\\Documents\\GitHub\\aicompression\\Slide Generator\\images_generated\\slide_4.jpg')
+    #image_difference = compare_image.compare_image()
+    #print(image_difference)
+    """
+    compression = compressor(PATH_TO_OD_LABELS="../models/object_detection/labels",PATH_TO_OD_MODEL_DIR="../models/object_detection")
+    detections = compression.detect_objects("C:\\Users\\François\\Desktop\\text_sample.jpg")
+    texts = compression.perform_ocr()
+    """
+    img = Image.open(r'C:\Users\François\Desktop\text_sample.jpg')
+    print(pytesseract.image_to_string(img))
+
